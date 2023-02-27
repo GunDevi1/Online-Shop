c8ecd0e9cf02d379e0bf48f297cbdcada5a70832
GunDevi1 <navidota588@gmail.com>
2023-02-27 20:42:32 +0200

    many changes

---
+++ js/slider.js
@@ -0,0 +1,35 @@
+new Swiper('.mySwiper', {
+	spaceBetween: 30,
+
+	loopedSlides: 0,
+
+	pagination: {
+		el: '.swiper-pagination',
+		dynamicBullets: true,
+	},
+
+	slidesPerView: 4,
+	loop: true,
+	autoplay: {
+		delay: 3000,
+	},
+
+	speed: 800,
+
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+		},
+		480: {
+			slidesPerView: 2,
+		},
+		978: {
+			slidesPerView: 3,
+		},
+		1024: {
+			slidesPerView: 4,
+		},
+	},
+
+	initialSlide: 3,
+})
