42e754c128adf9cba8ca9465908a55559b7b0ce8
GunDevi1 <navidota588@gmail.com>
2023-03-06 17:02:22 +0200

    big upgrade

--- js/slider.js
+++ js/slider.js
@@ -1,21 +1,16 @@
-new Swiper('.mySwiper', {
-	spaceBetween: 30,
-
+const addSwiper = new Swiper('.mySwiper', {
+	spaceBetween: 0,
 	loopedSlides: 0,
-
 	pagination: {
 		el: '.swiper-pagination',
 		dynamicBullets: true,
 	},
-
 	slidesPerView: 4,
 	loop: true,
 	autoplay: {
 		delay: 3000,
 	},
-
 	speed: 800,
-
 	breakpoints: {
 		320: {
 			slidesPerView: 1,
@@ -30,6 +25,5 @@ new Swiper('.mySwiper', {
 			slidesPerView: 4,
 		},
 	},
-
 	initialSlide: 3,
 })
